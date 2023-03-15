a1fb3de4a49bc94a2c840e6fdd879e34fcbc731c
kungurtsev <kungurtsev@skbkontur.ru>
2023-03-15 18:51:37 +0400

    fill offsets

--- Vostok.OpenTelemetry.Exporter.Hercules/Helpers/TagNames.cs
+++ Vostok.OpenTelemetry.Exporter.Hercules/Helpers/TagNames.cs
@@ -9,8 +9,10 @@ internal static class TagNames
     public const string ParentSpanId = "parentSpanId";
 
     public const string BeginTimestampUtc = "beginTimestampUtc";
+    public const string BeginTimestampUtcOffset = "beginTimestampUtcOffset";
 
     public const string EndTimestampUtc = "endTimestampUtc";
+    public const string EndTimestampUtcOffset = "endTimestampUtcOffset";
 
     public const string Annotations = "annotations";
 }
