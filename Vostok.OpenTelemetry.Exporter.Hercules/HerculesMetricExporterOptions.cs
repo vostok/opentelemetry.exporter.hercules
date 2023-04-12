133d612319a59c9668347e50fb0f63929d0ac985
kungurtsev <kungurtsev@skbkontur.ru>
2023-04-12 19:42:51 +0400

    export gauges

--- Vostok.OpenTelemetry.Exporter.Hercules/HerculesMetricExporterOptions.cs
+++ Vostok.OpenTelemetry.Exporter.Hercules/HerculesMetricExporterOptions.cs
@@ -5,5 +5,7 @@ namespace Vostok.OpenTelemetry.Exporter.Hercules;
 [PublicAPI]
 public class HerculesMetricExporterOptions
 {
+    public string FinalStream { get; set; } = "metrics_final";
+    
     public string CountersStream { get; set; } = "metrics_counters";
 }
