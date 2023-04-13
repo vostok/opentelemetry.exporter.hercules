ccc1a544f474a7fbbc891a0ed5cc5bb6dcf1aa53
kungurtsev <kungurtsev@skbkontur.ru>
2023-04-13 13:32:48 +0400

    delete useless base hercules exporter

--- Vostok.OpenTelemetry.Exporter.Hercules/HerculesActivityExporter.cs
+++ Vostok.OpenTelemetry.Exporter.Hercules/HerculesActivityExporter.cs
@@ -3,23 +3,28 @@ using System.Diagnostics;
 using JetBrains.Annotations;
 using OpenTelemetry;
 using Vostok.Hercules.Client.Abstractions;
-using Vostok.Hercules.Client.Abstractions.Events;
 using Vostok.OpenTelemetry.Exporter.Hercules.Builders;
 
 namespace Vostok.OpenTelemetry.Exporter.Hercules;
 
 [PublicAPI]
-public class HerculesActivityExporter : HerculesExporter<Activity>
+public class HerculesActivityExporter : BaseExporter<Activity>
 {
+    private readonly IHerculesSink sink;
     private readonly Func<HerculesActivityExporterOptions> optionsProvider;
 
     public HerculesActivityExporter(IHerculesSink sink, Func<HerculesActivityExporterOptions> optionsProvider)
-        : base(sink) =>
+    {
+        this.sink = sink;
         this.optionsProvider = optionsProvider;
+    }
 
-    protected override string SelectStream(Activity @event) =>
-        optionsProvider().Stream;
+    public override ExportResult Export(in Batch<Activity> batch)
+    {
+        foreach (var @event in batch)
+            sink.Put(optionsProvider().Stream, builder =>
+                HerculesActivityBuilder.Build(@event, ParentProvider!.GetResource(), builder, optionsProvider().FormatProvider));
 
-    protected override void BuildEvent(IHerculesEventBuilder builder, Activity @event) =>
-        HerculesActivityBuilder.Build(@event, ParentProvider!.GetResource(), builder, optionsProvider().FormatProvider);
+        return ExportResult.Success;
+    }
 }
