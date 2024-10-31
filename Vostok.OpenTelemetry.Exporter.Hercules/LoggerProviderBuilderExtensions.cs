866d7e066262574efa04816fb64f9a03950471db
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2024-10-31 16:37:18 +0200

    Add logger provider extensions

---
+++ Vostok.OpenTelemetry.Exporter.Hercules/LoggerProviderBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using Vostok.Hercules.Client.Abstractions;
+
+namespace Vostok.OpenTelemetry.Exporter.Hercules;
+
+[PublicAPI]
+public static class LoggerProviderBuilderExtensions
+{
+    public static LoggerProviderBuilder AddHerculesExporter(this LoggerProviderBuilder builder) =>
+        builder.AddHerculesExporter(null, null);
+
+    public static LoggerProviderBuilder AddHerculesExporter(this LoggerProviderBuilder builder, Action<HerculesLogExporterOptions>? configure) =>
+        builder.AddHerculesExporter(null, configure);
+
+    public static LoggerProviderBuilder AddHerculesExporter(this LoggerProviderBuilder builder, string? name, Action<HerculesLogExporterOptions>? configure)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        name ??= Options.DefaultName;
+
+        builder.ConfigureServices(services =>
+        {
+            if (configure != null)
+                services.Configure(name, configure);
+        });
+
+        builder.AddProcessor(serviceProvider =>
+        {
+            var optionsProvider = () => serviceProvider.GetRequiredService<IOptionsMonitor<HerculesLogExporterOptions>>().Get(name);
+
+            var sink = serviceProvider.GetRequiredService<IHerculesSink>();
+
+            return new SimpleLogRecordExportProcessor(new HerculesLogExporter(sink, optionsProvider));
+        });
+
+        return builder;
+    }
+}
