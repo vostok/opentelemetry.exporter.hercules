9d777278f1c296b8b1ffba913d637d226fd9c118
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2025-04-18 12:48:49 +0200

    Dispose log exporter.

--- Vostok.OpenTelemetry.Exporter.Hercules/HerculesLogExporter.cs
+++ Vostok.OpenTelemetry.Exporter.Hercules/HerculesLogExporter.cs
@@ -14,17 +14,28 @@ public sealed class HerculesLogExporter(IHerculesSink sink, Func<HerculesLogExpo
 {
     private Resource? _resource;
 
+    private bool _shutdownCalled;
+
     public override ExportResult Export(in Batch<LogRecord> batch)
     {
         _resource ??= ParentProvider.GetResource();
 
         var options = optionsProvider();
-        if (options.Enabled)
+        if (options.Enabled && !_shutdownCalled)
         {
             foreach (var logRecord in batch)
                 sink.Put(options.Stream, builder => builder.BuildLogRecord(logRecord, _resource, options.FormatProvider));
         }
 
         return ExportResult.Success;
+    }
+
+    // note (ponomaryovigor, 28.04.2025):
+    // Fix has been added only for this exporter for simplicity reasons (only logs exporting faces "use after dispose" issue). 
+    // Feel free to add same logic to activity and metric exporting if needed.
+    protected override bool OnShutdown(int timeoutMilliseconds)
+    {
+        _shutdownCalled = true;
+        return base.OnShutdown(timeoutMilliseconds);
     }
 }
